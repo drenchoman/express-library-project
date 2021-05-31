cee4d2eaf545709c4dbafdc6d5f916e351244a10
Oscar <>
2021-05-31 16:57:58 +1200

    Restart library project

---
+++ models/author.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose')
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema({
+  first_name: {type: String, required: true, maxLength: 100},
+  last_name: {type: String, required: true, maxLength: 100},
+  date_of_birth: {type: Date},
+  date_of_death: {type: Date},
+});
+
+// Virtuals
+
+AuthorSchema
+.virtual('name')
+.get(function () {
+  return this.first_name + ', ' + this.last_name;
+});
+
+// Lifespan
+
+AuthorSchema
+.virtual('Lifespan')
+.get(function () {
+  return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+});
+
+AuthorSchema
+.virtual('url')
+.get(function (){
+  return '/catalog/author/' + this._id;
+});
+
+module.exports = mongoose.model('Author', AuthorSchema);
